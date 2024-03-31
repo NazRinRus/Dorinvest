ca78b264047fed30bbe888639b5a41d8cf4e046c
NazRinRus <NazRinRus@gmail.com>
2024-03-31 12:07:25 +0300

    Первоначальная подготовка проекта

--- exhibition/admin.py
+++ exhibition/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from exhibition.models.exhibition import Exhibition
+from exhibition.models import exhibition, participant
 
-@admin.register(Exhibition)
-class OrganisationAdmin(admin.ModelAdmin):
+@admin.register(exhibition.Exhibition)
+class ExhibitionAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'bunner', 'date', 'location',)
+
+@admin.register(participant.Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'color', 'other', 'type_p', 'breed',)
+
+@admin.register(participant.TypeParticipant)
+class ParticipantTypeAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'color', 'sort', 'is_active',)
+
+@admin.register(participant.BreedParticipant)
+class ParticipantBreedAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'color', 'sort', 'is_active',)
