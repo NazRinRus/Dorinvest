7aa073a1f227b9b99f279d46f1fd27911c24e33a
NazRinRus <NazRinRus@gmail.com>
2024-03-31 10:37:20 +0300

    Первоначальная подготовка проекта

--- config/settings.py
+++ config/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS += [
 INSTALLED_APPS += [
     'api',
     'common',
+    'exhibition',
 ]
 
 # drf_spectacular apps
---
+++ exhibition/models/exhibition.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+class Exhibition(models.Model):
+    name = models.CharField("Название выставки", max_length=30, null=True)
+    bunner = models.ImageField("Баннер")
+    date = models.DateTimeField("Дата проведения")
+    location = models.CharField("Место проведения", max_length=30, null=False)
+
+    class Meta:
+        verbose_name = 'Выставка'
+        verbose_name_plural = 'Выставки'
+        ordering = ('date', 'name',)
