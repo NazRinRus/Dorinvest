24446842f00315af39b4c994587cff6276c99148
NazRinRus <NazRinRus@gmail.com>
2024-03-31 10:37:20 +0300

    модели готовы

--- exhibition/admin.py
+++ exhibition/admin.py
@@ -1,30 +1,6 @@
 from django.contrib import admin
-from exhibition.models import exhibition, participant, partner
+from exhibition.models.exhibition import Exhibition
 
-@admin.register(exhibition.Exhibition)
-class ExhibitionAdmin(admin.ModelAdmin):
+@admin.register(Exhibition)
+class OrganisationAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'bunner', 'date', 'location',)
-
-@admin.register(exhibition.Foto)
-class ExhibitionFotoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'foto', 'description',)
-
-@admin.register(participant.Participant)
-class ParticipantAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'color', 'other', 'type_p', 'breed', 'avatar_id',)
-
-@admin.register(participant.TypeParticipant)
-class ParticipantTypeAdmin(admin.ModelAdmin):
-    list_display = ('code', 'name', 'sort', 'is_active',)
-
-@admin.register(participant.BreedParticipant)
-class ParticipantBreedAdmin(admin.ModelAdmin):
-    list_display = ('code', 'name', 'sort', 'is_active',)
-
-@admin.register(participant.Avatar)
-class ParticipantBreedAdmin(admin.ModelAdmin):
-    list_display = ('id', 'foto', 'description',)
-
-@admin.register(partner.Partner)
-class PartnerAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'logo', 'info', 'website',)
--- exhibition/models/participant.py
+++ exhibition/models/participant.py
@@ -3,65 +3,13 @@ from django.db import models
 
 User = get_user_model()
 
-
-class TypeParticipant(models.Model):
-    code = models.CharField('Код', max_length=3, primary_key=True)
-    name = models.CharField("Вид животного", max_length=30, null=False)
-    sort = models.PositiveSmallIntegerField('Сортировка', null=True, blank=True)
-    is_active = models.BooleanField("Активность", default=True)
-
-    class Meta:
-        verbose_name = 'Вид животного'
-        verbose_name_plural = 'Виды животных'
-        ordering = ('name',)
-
-    def __str__(self):
-        return f'{self.name} ({self.code})'
-
-class BreedParticipant(models.Model):
-    code = models.CharField('Код', max_length=3, primary_key=True)
-    name = models.CharField("Порода", max_length=30, null=False)
-    sort = models.PositiveSmallIntegerField('Сортировка', null=True, blank=True)
-    is_active = models.BooleanField("Активность", default=True)
-
-    class Meta:
-        verbose_name = 'Порода'
-        verbose_name_plural = 'Породы'
-        ordering = ('name',)
-
-    def __str__(self):
-        return f'{self.name} ({self.code})'
-
-
 class Participant(models.Model):
-    name = models.CharField("Кличка", max_length=30, null=False)
-    color = models.CharField("Окрас", max_length=30, null=True)
-    other = models.TextField("Описание", null=True)
-    type_p = models.ForeignKey(
-        'TypeParticipant', models.RESTRICT, 'participant_type', verbose_name='Вид животного'
-    )
-    breed = models.ForeignKey(
-        'BreedParticipant', models.RESTRICT, 'participant_breed', verbose_name='Порода'
-    )
-    avatar_id = models.ForeignKey(
-        'Avatar', models.CASCADE, 'avatar_participant', verbose_name='Фотографии'
-    )
+    name = models.CharField("Название выставки", max_length=30, null=True)
+    bunner = models.ImageField("Баннер")
+    date = models.DateTimeField("Дата проведения")
+    location = models.CharField("Место проведения", max_length=30, null=False)
 
     class Meta:
-        verbose_name = 'Участник'
-        verbose_name_plural = 'Участники'
-        ordering = ('name',)
-
-    def __str__(self):
-        return f'{self.name} ({self.pk})'
-
-class Avatar(models.Model):
-    foto = models.CharField("Фотография", max_length=255, null=False)
-    description = models.CharField("Описание фотографии", max_length=255, null=True)
-
-    class Meta:
-        verbose_name = 'Фотография'
-        verbose_name_plural = 'Фотографии'
-
-    def __str__(self):
-        return f'{self.description} ({self.pk})'
+        verbose_name = 'Выставка'
+        verbose_name_plural = 'Выставки'
+        ordering = ('date', 'name',)
