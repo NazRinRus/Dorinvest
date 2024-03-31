f16711717c443cb22e0ed1a9aaac633d3db3a569
NazRinRus <NazRinRus@gmail.com>
2024-03-31 12:07:25 +0300

    Модели готовы

--- exhibition/admin.py
+++ exhibition/admin.py
@@ -1,30 +1,18 @@
 from django.contrib import admin
-from exhibition.models import exhibition, participant, partner
+from exhibition.models import exhibition, participant
 
 @admin.register(exhibition.Exhibition)
 class ExhibitionAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'bunner', 'date', 'location',)
 
-@admin.register(exhibition.Foto)
-class ExhibitionFotoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'foto', 'description',)
-
 @admin.register(participant.Participant)
 class ParticipantAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'color', 'other', 'type_p', 'breed', 'avatar_id',)
+    list_display = ('id', 'name', 'color', 'other', 'type_p', 'breed',)
 
 @admin.register(participant.TypeParticipant)
 class ParticipantTypeAdmin(admin.ModelAdmin):
-    list_display = ('code', 'name', 'sort', 'is_active',)
+    list_display = ('code', 'name', 'color', 'sort', 'is_active',)
 
 @admin.register(participant.BreedParticipant)
 class ParticipantBreedAdmin(admin.ModelAdmin):
-    list_display = ('code', 'name', 'sort', 'is_active',)
-
-@admin.register(participant.Avatar)
-class ParticipantBreedAdmin(admin.ModelAdmin):
-    list_display = ('id', 'foto', 'description',)
-
-@admin.register(partner.Partner)
-class PartnerAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'logo', 'info', 'website',)
+    list_display = ('code', 'name', 'color', 'sort', 'is_active',)
