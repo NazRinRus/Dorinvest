710bcad6727835c4efb5b03a10ff970497df875f
NazRinRus <NazRinRus@gmail.com>
2024-04-17 17:41:43 +0300

    Сделано: - API по всем выставкам, по конкретной, с фотографиями, участниками и спонсорами; - Добавить API -- для текущего или ближайшего мероприятия, для стартовой страницы; -- с шестью последними мероприятиями Сделать:

--- exhibition/urls.py
+++ exhibition/urls.py
@@ -6,9 +6,12 @@ from exhibition.views import exhibition
 router = DefaultRouter()
 
 router.register(r'foto', exhibition.FotoExhibitionViewSet, 'foto-exhibition')
+#router.register(r'now', exhibition.ExhibitionNowAPIRetrieve.as_view(), 'now-exhibition')
 
 urlpatterns = [
     path('exhibition/', exhibition.ExhibitionAPIList.as_view(), name='exhibitions'),
     path('exhibition/<int:pk>/', exhibition.ExhibitionAPIRetrieve.as_view(), name='exhibition'),
+    path('exhibition/now/', exhibition.ExhibitionNowAPIRetrieve.as_view({'get': 'list'}), name='now-exhibition'),
+    path('exhibition/previous/', exhibition.ExhibitionPreviousAPIView.as_view({'get': 'list'}), name='prev-exhibition'),
     path('exhibition/', include(router.urls)),
 ]
