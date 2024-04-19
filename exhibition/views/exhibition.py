455ac310b2d68853df0a61027328e36feb2d6b19
Hashtagich <antonchikz@mail.ru>
2024-04-19 12:44:36 +0300

    Добавление API. Добавил API по статистике пристроенных животных, как за все выставки, так и за конкретную.

--- exhibition/views/exhibition.py
+++ exhibition/views/exhibition.py
@@ -7,12 +7,19 @@ from rest_framework import viewsets, filters, generics, mixins
 from rest_framework.response import Response
 
 from exhibition.models.exhibition import Foto, Exhibition, FAQ, Feedback
+from exhibition.models.participant import TypeParticipant, Participant
 from exhibition.serializers.api.exhibition import FotoExhibitionSerializer, ExhibitionSerializer, FAQSerializer, \
     FeedbackSerializer
 from exhibition.service import send_message
 
 
+#####################
+###Exhibition########
+#####################
 class FotoExhibitionViewSet(viewsets.ModelViewSet):
+    """
+    API фото выставки.
+    """
     serializer_class = FotoExhibitionSerializer
     queryset = Foto.objects.all()
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
@@ -26,6 +33,9 @@ class FotoExhibitionViewSet(viewsets.ModelViewSet):
     get=extend_schema(summary='Информация о выставках', tags=['Выставки'])
 )
 class ExhibitionAPIList(generics.ListAPIView):
+    """
+    API для предоставления всех выставок.
+    """
     serializer_class = ExhibitionSerializer
     queryset = Exhibition.objects.all()
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
@@ -39,6 +49,9 @@ class ExhibitionAPIList(generics.ListAPIView):
     get=extend_schema(summary='Информация о выставке', tags=['Выставки'])
 )
 class ExhibitionAPIRetrieve(generics.RetrieveAPIView):
+    """
+    API для предоставления выставки по её ID.
+    """
     serializer_class = ExhibitionSerializer
     queryset = Exhibition.objects.all()
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
@@ -53,6 +66,9 @@ class ExhibitionAPIRetrieve(generics.RetrieveAPIView):
     get=extend_schema(summary='Ближайшая или текущая выставка', tags=['Выставки'])
 )
 class ExhibitionNowAPIRetrieve(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """
+    API для предоставления ближайшей/текущей выставок.
+    """
     serializer_class = ExhibitionSerializer
 
     def get_queryset(self):
@@ -64,6 +80,9 @@ class ExhibitionNowAPIRetrieve(viewsets.GenericViewSet, mixins.ListModelMixin):
     get=extend_schema(summary='6 прошедших выставок', tags=['Выставки'])
 )
 class ExhibitionPreviousAPIView(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """
+    API для предоставления 6 последних выставок.
+    """
     serializer_class = ExhibitionSerializer
 
     def get_queryset(self):
@@ -71,10 +90,77 @@ class ExhibitionPreviousAPIView(viewsets.GenericViewSet, mixins.ListModelMixin):
         return qs
 
 
+#####################
+###Statistics########
+#####################
+@extend_schema_view(
+    get=extend_schema(summary='Статистика по всем выставкам', tags=['Статистика'])
+)
+class FoundHomeSummaryAPIView(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """
+    API для статистики.
+    Отображается сколько всего участников/питомцев взяли домой.
+    Сортировка происходит по видам участников со статусом(is_active) True и в рамках всех выставок.
+    """
+    serializer_class = ExhibitionSerializer
+
+    def list(self, request, *args, **kwargs):
+        qs = {}
+
+        type_ps = TypeParticipant.objects.filter(is_active=True)
+
+        for type_p in type_ps:
+            total_found_home = Participant.objects.filter(type_p=type_p, found_home=True).count()
+            qs[type_p.name] = total_found_home
+
+        return Response(qs)
+
+
+@extend_schema_view(
+    get=extend_schema(summary='Статистика по конкретной выставке', tags=['Статистика'])
+)
+class FoundHomeSummaryDetailsAPIView(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """
+    API для статистики.
+    Отображается сколько всего участников/питомцев взяли домой.
+    Сортировка происходит по видам участников со статусом(is_active) True и в рамках конкретной выставки.
+    """
+    serializer_class = ExhibitionSerializer
+
+    def list(self, request, *args, **kwargs):
+        exhibition_id = kwargs.get('exhibition_id')  # извлекаем exhibition_id из kwargs
+
+        if not exhibition_id:
+            return Response({'message': 'Не указан идентификатор выставки'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            exhibition = Exhibition.objects.get(pk=exhibition_id)
+        except Exhibition.DoesNotExist:
+            return Response({'message': 'Выставка не найдена'}, status=status.HTTP_404_NOT_FOUND)
+
+        qs = {}
+        participants = exhibition.participants.filter(type_p__is_active=True)
+
+        for participant in participants:
+            total_found_home = Participant.objects.filter(
+                type_p=participant.type_p,
+                exhibition_participant=exhibition,
+                found_home=True,
+            ).count()
+
+            qs[participant.type_p.name] = total_found_home
+
+        return Response(qs)
+
+
+################
+###Other########
+################
 @extend_schema_view(
     get=extend_schema(summary='Часто задаваемые вопросы', tags=['FAQ'])
 )
 class FAQAPIList(generics.ListAPIView):
+    """API FAQ - часто задаваемые вопросы и ответы на инх."""
     serializer_class = FAQSerializer
     queryset = FAQ.objects.all()
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
